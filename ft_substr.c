152861201d670ad95f1a6d2c377f38eac83a93a1
高松浩大 <machi@takamatsuhiroshidainoMacBook-Air.local>
2023-06-15 18:00:40 +0900

    add all

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: machi <machi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 16:22:35 by machi             #+#    #+#             */
-/*   Updated: 2023/06/15 11:25:53 by machi            ###   ########.fr       */
+/*   Updated: 2023/06/15 11:46:49 by machi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	unsigned int	i;
-	char			*new;
+	char			*a;
 	size_t			sum;
 
 	if (!s)
@@ -25,15 +25,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		len = sum - start;
 	if (start >= sum || len == 0)
 		return (ft_strdup(""));
-	new = (char *)malloc(sizeof(char) * (len + 1));
+	a = (char *)malloc(sizeof(char) * (len + 1));
 	i = 0;
-	if (new == NULL)
+	if (a == NULL)
 		return (NULL);
 	while (i < len)
 	{
-		new[i] = s[start + i];
+		a[i] = s[start + i];
 		i++;
 	}
-	new[i] = '\0';
-	return (new);
+	a[i] = '\0';
+	return (a);
 }
